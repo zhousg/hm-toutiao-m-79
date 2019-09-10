4cbd0c308d07e704dd62a7eeb780a2ac74a319e6
zhousg <zhoushugang@itcast.cn>
2019-09-10 10:37:14 +0800

    引入vant&rem适配

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    'postcss-pxtorem': {
+      // 转换成rem的默认基准值  100px的容器   100/37.5rem的容器
+      // 37.5 因为vant是基于iphone6来做适配的  宽度375px
+      // 使用了一款 flexible 插件  设置基准值 html font-size 规则：基准值=当前设备宽带的十分之一
+      // 适配机型 iPhone5|5 320px宽度  32
+      // 适配机型 iPhone6|7|8 375px宽度  37.5
+      rootValue: 37.5,
+      propList: ['*']
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 使用vant组件库  (完整导入，将来做优化改成按需导入)
+import vant from 'vant'
+import 'vant/lib/index.css'
+
+import 'amfe-flexible'
+
+Vue.use(vant)
+
 Vue.config.productionTip = false
 
 new Vue({
